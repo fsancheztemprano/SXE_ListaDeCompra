7425772d026a8527def4d160d6a9f4224d213037
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2020-02-19 13:36:27 +0100

    2 - Opción de 'comprado', con lo que los productos pasan a la parte inferior

--- src/app/MockProductos.ts
+++ src/app/MockProductos.ts
@@ -4,6 +4,6 @@ export const PRODUCTOS: Producto[] = [
   {nombre: 'Pasta Dental', marca: 'Colgate', comprado: false},
   {nombre: 'Queso', marca: 'Larsa', comprado: false},
   {nombre: 'Pizza', marca: 'Buitoni', comprado: false},
-  {nombre: 'Agua', marca: 'Evian', comprado: false},
+  {nombre: 'Agua', marca: 'Evian', comprado: true},
   {nombre: 'Patatas', marca: 'Lays', comprado: false}
 ];
